f8b5d0808136e1659e7595fcf805f42769cb5bca
olegzig <boris37544@gmail.com>
2020-12-01 23:23:07 +0300

    Feth: Всё сломали к ебеням

--- WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/Form1.cs
@@ -8,6 +8,14 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 //DefaultImage - картинка которая ебашится на старте
+
+//PhysicConverterPanel - панель, на которой происходит выбор хуйни для фищики
+//ChemistryConverterPanel - панель, на которой происходит выбор хуйни для фищики
+
+//PhysicReaderPanel - панель, на которой происходит выбор хуйни для фищики
+//ChemistryReaderPanel - панель, на которой происходит выбор хуйни для фищики
+
+//PhysicBoxConverterRange - физика. Окно для расстояния
 namespace WindowsFormsApp1
 {
     public partial class Form1 : Form
