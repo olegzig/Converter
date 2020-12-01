3d7bdc6dc6b81319e61da680a82fbc9ae46acff2
olegzig <boris37544@gmail.com>
2020-12-01 22:47:41 +0300

    feth: изменил меню

--- WindowsFormsApp1/Form1.Designer.cs
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -29,10 +29,13 @@
         private void InitializeComponent()
         {
             this.menuStrip1 = new System.Windows.Forms.MenuStrip();
-            this.StripMenuForm = new System.Windows.Forms.ToolStripMenuItem();
-            this.StripMenuFormConverter = new System.Windows.Forms.ToolStripMenuItem();
-            this.StripMenuFormFileReader = new System.Windows.Forms.ToolStripMenuItem();
             this.StripMenuHelp = new System.Windows.Forms.ToolStripMenuItem();
+            this.конвертерToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.ридерToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.MenuConverterPhysics = new System.Windows.Forms.ToolStripMenuItem();
+            this.MenuConverterChemistry = new System.Windows.Forms.ToolStripMenuItem();
+            this.MenuReaderPhysics = new System.Windows.Forms.ToolStripMenuItem();
+            this.MenuReaderChemistry = new System.Windows.Forms.ToolStripMenuItem();
             this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -40,8 +43,9 @@
             // 
             this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
             this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.StripMenuForm,
-            this.StripMenuHelp});
+            this.StripMenuHelp,
+            this.конвертерToolStripMenuItem,
+            this.ридерToolStripMenuItem});
             this.menuStrip1.Location = new System.Drawing.Point(0, 0);
             this.menuStrip1.Name = "menuStrip1";
             this.menuStrip1.Padding = new System.Windows.Forms.Padding(14, 3, 0, 3);
@@ -49,32 +53,53 @@
             this.menuStrip1.TabIndex = 0;
             this.menuStrip1.Text = "menuStrip1";
             // 
-            // StripMenuForm
-            // 
-            this.StripMenuForm.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.StripMenuFormConverter,
-            this.StripMenuFormFileReader});
-            this.StripMenuForm.Name = "StripMenuForm";
-            this.StripMenuForm.Size = new System.Drawing.Size(57, 19);
-            this.StripMenuForm.Text = "Форма";
-            // 
-            // StripMenuFormConverter
-            // 
-            this.StripMenuFormConverter.Name = "StripMenuFormConverter";
-            this.StripMenuFormConverter.Size = new System.Drawing.Size(180, 22);
-            this.StripMenuFormConverter.Text = "Конвертер";
-            // 
-            // StripMenuFormFileReader
-            // 
-            this.StripMenuFormFileReader.Name = "StripMenuFormFileReader";
-            this.StripMenuFormFileReader.Size = new System.Drawing.Size(180, 22);
-            this.StripMenuFormFileReader.Text = "Ридер";
-            // 
             // StripMenuHelp
             // 
             this.StripMenuHelp.Name = "StripMenuHelp";
             this.StripMenuHelp.Size = new System.Drawing.Size(68, 19);
             this.StripMenuHelp.Text = "Помощь";
+            // 
+            // конвертерToolStripMenuItem
+            // 
+            this.конвертерToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.MenuConverterPhysics,
+            this.MenuConverterChemistry});
+            this.конвертерToolStripMenuItem.Name = "конвертерToolStripMenuItem";
+            this.конвертерToolStripMenuItem.Size = new System.Drawing.Size(77, 19);
+            this.конвертерToolStripMenuItem.Text = "Конвертер";
+            // 
+            // ридерToolStripMenuItem
+            // 
+            this.ридерToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.MenuReaderPhysics,
+            this.MenuReaderChemistry});
+            this.ридерToolStripMenuItem.Name = "ридерToolStripMenuItem";
+            this.ридерToolStripMenuItem.Size = new System.Drawing.Size(52, 19);
+            this.ридерToolStripMenuItem.Text = "Ридер";
+            // 
+            // MenuConverterPhysics
+            // 
+            this.MenuConverterPhysics.Name = "MenuConverterPhysics";
+            this.MenuConverterPhysics.Size = new System.Drawing.Size(180, 22);
+            this.MenuConverterPhysics.Text = "Физика";
+            // 
+            // MenuConverterChemistry
+            // 
+            this.MenuConverterChemistry.Name = "MenuConverterChemistry";
+            this.MenuConverterChemistry.Size = new System.Drawing.Size(180, 22);
+            this.MenuConverterChemistry.Text = "Химия";
+            // 
+            // MenuReaderPhysics
+            // 
+            this.MenuReaderPhysics.Name = "MenuReaderPhysics";
+            this.MenuReaderPhysics.Size = new System.Drawing.Size(180, 22);
+            this.MenuReaderPhysics.Text = "Физика";
+            // 
+            // MenuReaderChemistry
+            // 
+            this.MenuReaderChemistry.Name = "MenuReaderChemistry";
+            this.MenuReaderChemistry.Size = new System.Drawing.Size(180, 22);
+            this.MenuReaderChemistry.Text = "Химия";
             // 
             // Form1
             // 
@@ -99,10 +124,13 @@
         #endregion
 
         private System.Windows.Forms.MenuStrip menuStrip1;
-        private System.Windows.Forms.ToolStripMenuItem StripMenuForm;
-        private System.Windows.Forms.ToolStripMenuItem StripMenuFormConverter;
-        private System.Windows.Forms.ToolStripMenuItem StripMenuFormFileReader;
         private System.Windows.Forms.ToolStripMenuItem StripMenuHelp;
+        private System.Windows.Forms.ToolStripMenuItem конвертерToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem MenuConverterPhysics;
+        private System.Windows.Forms.ToolStripMenuItem MenuConverterChemistry;
+        private System.Windows.Forms.ToolStripMenuItem ридерToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem MenuReaderPhysics;
+        private System.Windows.Forms.ToolStripMenuItem MenuReaderChemistry;
     }
 }
 
