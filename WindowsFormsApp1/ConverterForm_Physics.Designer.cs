3d743c0005166a06c5bce3f07f3c16c2370f9d66
olegzig <boris37544@gmail.com>
2020-12-02 12:12:11 +0300

    feat: edned menu

--- WindowsFormsApp1/ConverterForm_Physics.Designer.cs
+++ WindowsFormsApp1/ConverterForm_Physics.Designer.cs
@@ -28,10 +28,19 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // ConverterForm_Physics
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "ConverterForm_Math";
+            this.ControlBox = false;
+            this.Name = "ConverterForm_Physics";
+            this.Text = "ConverterForm_Physics";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.ResumeLayout(false);
+
         }
 
         #endregion
--- WindowsFormsApp1/ReaderFormPhysics.Designer.cs
+++ WindowsFormsApp1/ReaderFormPhysics.Designer.cs
@@ -28,10 +28,19 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // ReaderFormPhysics
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ControlBox = false;
+            this.Name = "ReaderFormPhysics";
             this.Text = "ReaderFormPhysics";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.ResumeLayout(false);
+
         }
 
         #endregion
