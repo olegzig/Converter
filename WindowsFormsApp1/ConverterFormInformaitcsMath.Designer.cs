ced27af28628e476794848e681a841cd841746ad
olegzig <boris37544@gmail.com>
2020-12-08 12:04:59 +0300

    fix: window icon fixed

--- WindowsFormsApp1/ConverterFormInformaitcsMath.Designer.cs
+++ WindowsFormsApp1/ConverterFormInformaitcsMath.Designer.cs
@@ -115,6 +115,7 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ControlBox = false;
             this.Controls.Add(this.button2);
             this.Controls.Add(this.comboBox2);
             this.Controls.Add(this.textBox2);
