f0f3382e6b024c928dad2c672d25adc822857571
olegzig <boris37544@gmail.com>
2021-01-29 09:31:21 +0300

    snth changed

--- WindowsFormsApp1/Tasks/TaskForm.cs
+++ WindowsFormsApp1/Tasks/TaskForm.cs
@@ -12,7 +12,7 @@ namespace WindowsFormsApp1.Tasks
 {
     public partial class TaskForm : Form
     {
-        private string[] rightAws = new string[] { "Senjor Le Costylio", "1", "2", "3", "4" };
+        private string[] rightAws = new string[] { "Senjor Le Costylio", "1", "7853,9", "3", "4" };
         bool shitHappens = true;
 
         public TaskForm()
@@ -25,19 +25,19 @@ namespace WindowsFormsApp1.Tasks
             switch (RichID.ID)
             {
                 case 1:
-                    richTextBox1.Text = "1";
+                    richTextBox1.Text = "Дано:";//p1
                     break;
 
                 case 2:
-                    richTextBox1.Text = "2";
+                    richTextBox1.Text = "Дано: \nНайти: .\nФормулы: ";//m1
                     break;
 
                 case 3:
-                    richTextBox1.Text = "3";
+                    richTextBox1.Text = "phys2";//p2
                     break;
 
                 case 4:
-                    richTextBox1.Text = "4";
+                    richTextBox1.Text = "math2";//m2
                     break;
             }
         }
