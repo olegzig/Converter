e687f1fa43cca518ffc7a89575c46d61e716a752
olegzig <boris37544@gmail.com>
2021-01-29 11:46:09 +0300

    tasks filled

--- WindowsFormsApp1/Tasks/TaskForm.cs
+++ WindowsFormsApp1/Tasks/TaskForm.cs
@@ -12,7 +12,7 @@ namespace WindowsFormsApp1.Tasks
 {
     public partial class TaskForm : Form
     {
-        private string[] rightAws = new string[] { "Senjor Le Costylio", "1", "7853,9", "3", "4" };
+        private string[] rightAws = new string[] { "Senjor Le Costylio", "5", "400", "41,87", "12" };
         bool shitHappens = true;
 
         public TaskForm()
@@ -25,19 +25,19 @@ namespace WindowsFormsApp1.Tasks
             switch (RichID.ID)
             {
                 case 1:
-                    richTextBox1.Text = "Дано:";//p1
+                    richTextBox1.Text = "Дано: Груз весит 5000кг.\nНайти: его вес выраженный в тоннах.";//p1
                     break;
 
                 case 2:
-                    richTextBox1.Text = "Дано: \nНайти: .\nФормулы: ";//m1
+                    richTextBox1.Text = "Дано: квадрат со стороной 2см.\nНайти: площадь квадрата выраженную в мм^2.\nФормулы: площадь квадрата = a * a, где а - сторона квадрата";//m1
                     break;
 
                 case 3:
-                    richTextBox1.Text = "phys2";//p2
+                    richTextBox1.Text = "Дано: 10 калорий.\nНайти: их эквивалент в джоулях.\n";//p2
                     break;
 
                 case 4:
-                    richTextBox1.Text = "math2";//m2
+                    richTextBox1.Text = "Дано: Треугольник с стороной 40 см, и высотой к стороне в 60 см .\nНайти: площадь треугольника выраженную в дм^2 .\nФормулы: Площадь = (1/2)*ah, где а - сторона, h - высота";//m2
                     break;
             }
         }
